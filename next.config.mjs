5114897898602798d25fc76d3c2286b830017e29
PinkLaura <laura_nesossi@protonmail.com>
2024-10-24 11:55:32 +0200

    prod

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    basePath: "/my-portfolio",
 
-    output: "export",  // <=== enables static exports
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/my-portfolio/' : '',
+  basePath: isProd ? '/my-portfolio' : '',
+  output: 'export'
 };
+
 
 export default nextConfig;
