b102e0d1ad8987c1cb6020f9a9c66840041a78e7
PinkLaura <laura_nesossi@protonmail.com>
2024-10-24 18:11:13 +0200

    Footer fixed
    typo and fra hints

--- components/footer.js
+++ components/footer.js
@@ -6,11 +6,9 @@ export default function Footer() {
                     Realized with ♥ by Laura Nesossi
                 </p>
                 <p className="mb-2">
-                    For any comments or adviice contact me at <a href="mailto:laura_nesossi@protonmail.com">laura_nesossi@protonmail.com</a>.
+                    For any comments or advice contact me at <a href="mailto:laura_nesossi@protonmail.com">laura_nesossi@protonmail.com</a>.
                 </p>
-                <p>
-                    I give consent to process my data with the purpose of the recruitment process, in accordance to the Regulation of the European Parliament 679/2016, regarding the protection of natural persons and free movement of such data
-                </p>
+
             </div>
         </footer>
 
