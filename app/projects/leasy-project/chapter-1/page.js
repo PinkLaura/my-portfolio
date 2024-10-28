c3167b900280737dc5a353773f02c06cc646da95
PinkLaura <laura_nesossi@protonmail.com>
2024-10-28 18:00:43 +0100

    chapter 1

--- app/projects/leasy-project/chapter-1/page.js
+++ app/projects/leasy-project/chapter-1/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import LeasyHeaderSection from "@/components/leasy-header-section";
-
+import LeasyParagraph from "@/components/leasy-paragraph";
 
 export default function LeasyChapter3() {
     return (
@@ -9,32 +9,195 @@ export default function LeasyChapter3() {
             <LeasyHeaderSection
                 icon={
                     <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M6 21V27.6C6 28.0971 6.40294 28.5 6.9 28.5H15" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                        <path d="M28.5 21V27.6C28.5 28.0971 28.0971 28.5 27.6 28.5H21" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                        <path d="M21 7.5H27.6C28.0971 7.5 28.5 7.90294 28.5 8.4V15" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                        <path d="M6 15V8.4C6 7.90294 6.40294 7.5 6.9 7.5H15" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                        <path d="M21 28.5V30C21 31.6569 19.6569 33 18 33C16.3431 33 15 31.6569 15 30V28.5" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                        <path d="M6 15H7.5C9.15686 15 10.5 16.3431 10.5 18C10.5 19.6569 9.15686 21 7.5 21H6" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                        <path d="M28.5 15H30C31.6569 15 33 16.3431 33 18C33 19.6569 31.6569 21 30 21H28.5" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                        <path d="M21 7.5V6C21 4.34314 19.6569 3 18 3C16.3431 3 15 4.34314 15 6V7.5" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                        <path d="M25.499 25.5005L31.499 31.5005" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" />
+                        <path d="M4.49902 16.499C4.49902 23.1265 9.8716 28.499 16.499 28.499C19.8185 28.499 22.8232 27.1513 24.9956 24.973C27.1606 22.8023 28.499 19.807 28.499 16.499C28.499 9.8716 23.1265 4.49902 16.499 4.49902C9.8716 4.49902 4.49902 9.8716 4.49902 16.499Z" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" />
                     </svg>
 
+
                 }
-                title={"Crafting a Design System"}
-                number={'4'}
+                title={"Product Discovery"}
+                number={'1'}
                 paragraphs={
                     <>
-                        <p className="mb-2">After the launch of the MVP, I took time to rationalize the components into a design system. The MVP was launched in one month, primarily focused on core functionalities, but it miss coherency and structure.
+                        <p className="mb-2">
+                            When the company initially decided to pivot, I was asked to facilitate the decision-making process regarding the new product.
                         </p>
-                        <p>After the first two months of use, I analyzed the feedback and structured a comprehensive design system, improving both the UI/UX of the pages and the Figma file structure. This helped us iterate faster with more coherent design choices.
+                        <p>
+                            While the COO focused on understanding product-market fit and the business aspects, I helped <b>identify gaps in the rental digital environment</b>.
                         </p>
                     </>
                 }
-                maxW={"7"}
             />
 
             <section>
                 <div className="container max-w-6xl px-6 md:my-32 flex flex-col md:gap-24 gap-12 relative">
+
+                    {/* Conducting workshops */}
+                    <div className="flex flex-col md:flex-row md:justify-between gap-6">
+
+                        <div className="md:w-7/12">
+                            <LeasyParagraph
+                                title={"From FidoRent to... what?"}
+                                subtitle={"brainstorming and analysis"}
+                                text={
+                                    <>
+                                        <p className="mb-2">
+                                            As a first step, we identified the strengths we already had—from technology and knowledge to user experience, digitalization, and domain expertise in the real estate and rental market.                                         </p>
+                                        <p className="mb-2">
+                                            We needed to <b>leverage this expertise</b> to bring value to our users.                                        </p>
+                                    </>
+                                }
+                            />
+                        </div>
+
+                        <img src="https://pinklaura.github.io/my-portfolio/img/projects/leasy/chapter-1/knowledge.svg"
+                            className="md:w-4/12" />
+
+                    </div>
+
+                    {/* Conducting workshops pt2 */}
+                    <div className="flex flex-col md:flex-row md:justify-between gap-6">
+
+                        <img src="https://pinklaura.github.io/my-portfolio/img/projects/leasy/chapter-1/tenant-drivers.png"
+                            className="md:w-5/12 " />
+
+                        <div className="md:w-6/12">
+                            <LeasyParagraph
+                                text={
+                                    <>
+                                        <p className="mb-2">
+                                            Initially, we focused on the tenant side of the rental market.
+                                        </p>
+                                        <p className="mb-2">
+                                            After identifying a few potential solutions through brainstorming, we analyzed their value for both users and our business.
+                                        </p>
+                                        <p>
+                                            However, after gathering data, <b>we realized that the real opportunity lay with landlords and property managers</b>.
+                                        </p>
+                                    </>
+                                }
+                            />
+
+                            <div className="bg-indigo-100 p-6 pr-14 rounded-xl dashed-border-m drop-shadow-2 text-paragraph mt-12">
+
+                                <div className="absolute -top-2 -right-2 p-3 bg-white drop-shadow-2 -rotate-[15deg] stroke-leasy rounded-[8px] overflow-hidden">
+                                    <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M8.69702 15.6969C13.5717 15.6969 15.697 13.6459 15.697 8.6969C15.697 13.6459 17.8074 15.6969 22.697 15.6969C17.8074 15.6969 15.697 17.8073 15.697 22.6969C15.697 17.8073 13.5717 15.6969 8.69702 15.6969Z" strokeLinejoin="round" />
+                                        <path d="M2.69702 7.1969C5.83078 7.1969 7.19702 5.87843 7.19702 2.6969C7.19702 5.87843 8.55371 7.1969 11.697 7.1969C8.55371 7.1969 7.19702 8.55359 7.19702 11.6969C7.19702 8.55359 5.83078 7.1969 2.69702 7.1969Z" strokeLinejoin="round" />
+                                    </svg>
+
+
+                                </div>
+
+                                We decided to provide a solution to <b>simplify the life of big landlords and property managers</b>.
+                            </div>
+                        </div>
+
+                    </div>
+
+                    {/* Flow */}
+                    <div className="flex flex-col md:flex-row md:justify-between items-center gap-6">
+
+                        <div className="md:w-6/12">
+                            <LeasyParagraph
+                                title={"What does a property manager do?"}
+                                subtitle={"research and journey mapping"}
+                                text={
+                                    <>
+                                        <p className="mb-2">
+                                            My personal knowledge of the property manager's role was limited, so I conducted research to gain a deeper understanding of the field.
+                                        </p>
+                                        <p className="mb-2">
+                                            I studied market reports and data, and we conducted a survey to explore how Italian property managers perceive their roles and the activities they perform.
+                                        </p>
+                                        <p>
+                                            After the research, I created a <b>map of all the activities a property manager undertakes</b> to identify the most promising <b>opportunities for automation and digitalization</b>
+                                        </p>
+                                    </>
+                                }
+                            />
+                        </div>
+
+                        <img src="https://pinklaura.github.io/my-portfolio/img/projects/leasy/chapter-1/process.svg"
+                            className="md:w-5/12 drop-shadow-2 " />
+
+                    </div>
+
+                    <img src="https://pinklaura.github.io/my-portfolio/img/projects/leasy/chapter-1/features-map.svg" />
+
+                    {/* screen */}
+                    <div className="flex flex-col gap-8">
+
+                        <div className="md:w-10/12">
+                            <LeasyParagraph
+                                title={"What there is out there?"}
+                                subtitle={"Competitors analysis"}
+                                text={
+                                    <>
+                                        <p className="mb-2">
+                                            Last but not least, I <b>researched and compared the most significant competitors</b>: Italian companies to identify market gaps and American platforms—representing a more advanced market—for inspiration.                                         </p>
+                                        <p>
+                                            To better understand the software actually used by property managers, I conducted a deep dive into <b>Facebook groups</b> and discussions regarding software and property management issues.                                        </p>
+                                    </>
+                                }
+                            />
+                        </div>
+
+                        <div className="flex flex-col md:flex-row gap-6">
+                            <div className="w-full flex flex-col gap-4 text-center">
+                                <img src="https://pinklaura.github.io/my-portfolio/img/projects/leasy/chapter-1/facebook.png"
+                                    className="w-full" />
+                                <p className="text-base text-gray-400 italic">
+                                    Screenshots from Facebook groups
+                                </p>
+                            </div>
+
+                            <div className="w-full flex flex-col gap-4 text-center">
+                                <img src="https://pinklaura.github.io/my-portfolio/img/projects/leasy/chapter-1/competitors.png"
+                                    className="w-full" />
+                                <p className="text-base text-gray-400 italic">
+                                    Competitors analysis
+                                </p>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    {/* Final decision */}
+                    <div className="flex flex-col md:flex-row md:justify-between items-center gap-6">
+
+                        <div className="md:w-6/12">
+                            <LeasyParagraph
+                                title={"Final decision"}
+                                subtitle={"MVP scope defining"}
+                                text={
+                                    <>
+                                        <p>
+                                            We decided to build a rent management platform to assist property managers and landlords with both financial and administrative tasks.
+                                        </p>
+                                    </>
+                                }
+                            />
+                        </div>
+
+
+                        <div className="bg-indigo-100 p-6 pr-14 rounded-xl dashed-border-m drop-shadow-2 text-paragraph md:w-5/12">
+
+                            <div className="absolute -top-2 -right-2 p-3 bg-white drop-shadow-2 -rotate-[15deg] stroke-leasy rounded-[8px] overflow-hidden">
+                                <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M8.69702 15.6969C13.5717 15.6969 15.697 13.6459 15.697 8.6969C15.697 13.6459 17.8074 15.6969 22.697 15.6969C17.8074 15.6969 15.697 17.8073 15.697 22.6969C15.697 17.8073 13.5717 15.6969 8.69702 15.6969Z" strokeLinejoin="round" />
+                                    <path d="M2.69702 7.1969C5.83078 7.1969 7.19702 5.87843 7.19702 2.6969C7.19702 5.87843 8.55371 7.1969 11.697 7.1969C8.55371 7.1969 7.19702 8.55359 7.19702 11.6969C7.19702 8.55359 5.83078 7.1969 2.69702 7.1969Z" strokeLinejoin="round" />
+                                </svg>
+
+
+                            </div>
+
+                            For the MVP, we decided to concentrate on a single feature that could deliver value in multiple ways for both tenants and landlords: <b>automated rent collection with reminders</b>.
+                        </div>
+
+                    </div>
+
 
                 </div>
             </section>
