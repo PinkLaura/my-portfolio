716954f33ee0be2a5aed49f5f30c911b27886e74
PinkLaura <laura_nesossi@protonmail.com>
2024-10-31 01:27:56 +0100

    skyscanner update

--- app/projects/skyscanner/page.js
+++ app/projects/skyscanner/page.js
@@ -15,10 +15,17 @@ export default function skyscannerProject() {
                 <div className="w-min p-[18px] rounded-[18px] drop-shadow-[0_0_9px_rgba(0,0,0,0.25)] bg-white stroke-sky-500 relative z-20 
             hover:rotate-12 hover:scale-110 transition-all">
 
-                    <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M4.5 26.25L9 4.5H19.5C28.5 4.5 28.5 18 19.5 18H13.05L11.25 26.25H4.5Z" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                        <path d="M10.2002 31.5L14.7002 9.75H25.2001C34.2001 9.75 34.2001 23.25 25.2001 23.25H18.7501L16.9501 31.5H10.2002Z" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                    <svg width="37" height="36" viewBox="0 0 37 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g clip-path="url(#clip0_1092_22570)">
+                            <path d="M7.49947 9.34151L11.6482 14.9811C11.8321 15.231 11.8742 15.5581 11.7597 15.8465L6.90596 28.0715C6.7915 28.3599 6.83366 28.687 7.01748 28.9369L7.81657 30.0231C8.16358 30.4948 8.86138 30.5148 9.23477 30.0638L15.653 22.3121C16.0264 21.8611 16.7242 21.8811 17.0712 22.3528L19.7015 25.9283C19.8554 26.1375 19.9114 26.4027 19.8554 26.6561L18.9253 30.8602C18.728 31.7522 19.8314 32.3368 20.4585 31.6725L23.8658 28.0636C23.94 27.985 24.028 27.9203 24.1251 27.8729L28.5848 25.6944C29.4055 25.2936 29.176 24.0662 28.2657 23.9889L23.9753 23.6252C23.7168 23.6034 23.4802 23.4709 23.3263 23.2617L20.696 19.6862C20.349 19.2145 20.5376 18.5424 21.0794 18.3202L30.3904 14.5007C30.9321 14.2785 31.1207 13.6064 30.7737 13.1347L29.9746 12.0485C29.7908 11.7986 29.4911 11.661 29.1817 11.6844L16.066 12.6786C15.7566 12.702 15.4569 12.5644 15.273 12.3145L11.1243 6.67492C10.3879 5.67395 8.97952 5.45947 7.97858 6.19581C6.97765 6.93214 6.76311 8.34054 7.49947 9.34151Z"  strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                        </g>
+                        <defs>
+                            <clipPath id="clip0_1092_22570">
+                                <rect width="36" height="36" fill="white" transform="translate(0.25)" />
+                            </clipPath>
+                        </defs>
                     </svg>
+
 
 
 
@@ -53,13 +60,13 @@ export default function skyscannerProject() {
 
 
 const slides = [
-    
+
 
     {
-        src: "https://pinklaura.github.io/my-portfolio/img/projects/skyscanner/skyscanner-cover.png",
+        src: "https://pinklaura.github.io/my-portfolio/img/projects/skyscanner/skyscanner-cover.webp",
         alt: "slide 1"
     },
-    
+
     {
         src: "https://pinklaura.github.io/my-portfolio/img/projects/skyscanner/slide-02.webp",
         alt: "slide 2"
@@ -137,5 +144,5 @@ const slides = [
         src: "https://pinklaura.github.io/my-portfolio/img/projects/skyscanner/slide-16.webp",
         alt: "slide 16"
     },
-    
+
 ]
